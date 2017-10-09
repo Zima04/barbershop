cb4d812d4782658df669c1ba99c7d7586cc3ef72
Maxim Aderiha <fpm.aderiha@bsu.by>
2017-10-09 03:02:06 +0300

    changes for compatibility with ie

--- js/popups.js
+++ js/popups.js
@@ -1,40 +1,39 @@
-'use strict';
+const overlay = document.querySelector('.modal-overlay');
+const popupLogin = document.querySelector('.modal-content-form');
+const openPopupLoginBtn = document.querySelector('.login');
+const closePopupLoginBtn = popupLogin.querySelector('.modal-content-close-button');
+const login = popupLogin.querySelector('[name=login]');
+const password = popupLogin.querySelector('[name=password]');
+const form = popupLogin.querySelector('form');
+const popupMap = document.querySelector('.modal-content-map');
+const openPopupMapBtn = document.querySelector('.js-open-map');
+const closePopupMapBtn = document.querySelector('.js-close-map');
 
-let overlay = document.querySelector('.modal-overlay');
-let popupLogin = document.querySelector('.modal-content-form');
-let openPopupLoginBtn = document.querySelector('.login');
-let closePopupLoginBtn = popupLogin.querySelector('.modal-content-close-button');
-let login = popupLogin.querySelector('[name=login]');
-let password = popupLogin.querySelector('[name=password]');
-let form = popupLogin.querySelector('form');
-
-console.log(openPopupLoginBtn);
-
-openPopupLoginBtn.addEventListener('click', (event) => {
+openPopupLoginBtn.addEventListener('click', function (event) {
     event.preventDefault();
     overlay.classList.add('content-show');
     popupLogin.classList.add('content-show', 'show-animation');
-    setTimeout(() => {
+    setTimeout(function () {
         popupLogin.classList.remove('show-animation');
     }, 600);
     login.focus();
 });
 
-closePopupLoginBtn.addEventListener('click', (event) => {
+closePopupLoginBtn.addEventListener('click', function (event) {
     closePopupLogin(event);
 });
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function (event) {
     if (!login.value || !password.value) {
         event.preventDefault();
         popupLogin.classList.add('modal-error');
-        setTimeout(() => {
+        setTimeout(function() {
             popupLogin.classList.remove('modal-error');
         }, 600);
     }
 });
 
-window.addEventListener('keydown', (event) => {
+window.addEventListener('keydown', function (event) {
     if (event.keyCode === 27) {
         closePopupLogin(event);
     }
@@ -48,21 +47,17 @@ function closePopupLogin(event) {
     password.value = '';
 }
 
-let popupMap = document.querySelector('.modal-content-map');
-let openPopupMapBtn = document.querySelector('.js-open-map');
-let closePopupMapBtn = document.querySelector('.js-close-map');
-
-if (openPopupMapBtn) openPopupMapBtn.addEventListener('click', (event) => {
+if (openPopupMapBtn) openPopupMapBtn.addEventListener('click', function (event) {
     event.preventDefault();
     overlay.classList.add('content-show');
     popupMap.classList.add('content-show', 'show-animation');
 });
 
-if (closePopupMapBtn) closePopupMapBtn.addEventListener('click', (event) => {
+if (closePopupMapBtn) closePopupMapBtn.addEventListener('click', function (event) {
     closePopupMap(event);
 });
 
-window.addEventListener('keydown', (event) => {
+window.addEventListener('keydown', function (event) {
     if (event.keyCode === 27) {
         closePopupMap(event);
     }
